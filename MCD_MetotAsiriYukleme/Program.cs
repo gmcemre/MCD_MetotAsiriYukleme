2bfbf9da8c3879e067108842453cecef89759bcd
103AKSAM_EMRE <103AKSAM_EMRE@DESKTOP-TUMHS1A>
2021-11-26 15:59:07 +0300

    Add project files.

---
+++ MCD_MetotAsiriYukleme/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_MetotAsiriYukleme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal toplam = Topla(12M, 45M);
+            EkrandaGoster(toplam);
+            
+            EkrandaGoster(Topla(30,20));
+
+            Topla("Emre", "Gemici");
+
+            Console.Read();
+        }
+
+        static void EkrandaGoster (object o)
+        {
+            Console.WriteLine(o.ToString());
+        }
+
+        static int Topla(int sayi1,int sayi2)
+        {
+            int toplam = sayi1 + sayi2;
+            return toplam; 
+        }
+
+        static decimal  Topla(decimal sayi1,decimal sayi2)
+        {
+            decimal toplam = sayi1 + sayi2;
+            return toplam;
+        }
+
+        static string Topla(string metin1,string metin2)
+        {
+            return metin1 + " " + metin2;
+        }
+    }
+}
